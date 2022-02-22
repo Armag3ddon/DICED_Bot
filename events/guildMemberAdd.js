564ce595cb5c71cee1d6f531726ded4bb75d6f2c
armag3ddon <armag3ddon@web.de>
2022-02-22 09:59:07 +0100

    Complete the welcoming message with the guildMemberAdd event.

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,24 @@
+/* discord.js guildMemberAdd is fired when a user joins a guild */
+
+const DiscordLog = require('../help/log.js');
+
+module.exports = {
+	name: 'guildMemberAdd',
+	async execute(member) {
+		try {
+			let message = await member.client.DICED.messages_scheme.findOne({ where: { tag: 'willkommen' } });
+			if (message) {
+				const channel = await member.client.channels.fetch(process.env.WELCOME_CHANNEL);
+				message = message.replace('%%%', `<@${member.id}>`);
+				channel.send(message);
+			}
+			else {
+				DiscordLog(member.client, `Eine neue Person ist beigetreten (${member.user.username}), konnte aber nicht begrüßt werden, da keine Willkommensnachricht eingestellt ist.`);
+				throw 'guildMemberAdd: Missing welcome message.';
+			}
+		}
+		catch (error) {
+			console.error(error);
+		}
+	},
+};
